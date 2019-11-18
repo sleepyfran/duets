51ce0663cd1ea363e91a92356acea6a6bea938be
Fran González <fgonzaleva@icloud.com>
2019-11-18 19:48:04 +0100

    :fire: Remove unused nameOf

--- src/core/utils/nameof.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Creates a method that acts similar to C#'s nameof operator but allowing only keys belonging to the type T.
- */
-export const createNameOfType = <T>() => (name: keyof T) => name
