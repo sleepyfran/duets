0eb836ba2eb49f4d8c9817f9e10219a3b76a2116
sleepyfran <fgonzaleva@icloud.com>
2020-09-21 09:02:11 +0200

    ♻ Change confirmation input to use Dialoguer's confirmation

--- crates/cli/src/shared/display/prompts/confirmation_input_display.rs
+++ crates/cli/src/shared/display/prompts/confirmation_input_display.rs
@@ -1,6 +1,6 @@
-use super::text_choice_input_display;
+use dialoguer::Confirm;
 
-use crate::shared::action::{Choice, CliAction, ConfirmationChoice};
+use crate::shared::action::{CliAction, ConfirmationChoice};
 use crate::shared::context::Context;
 
 /// Reuses the text_choice_input_display module to avoid repeating the cumbersome process of
@@ -10,27 +10,15 @@ pub fn handle(
     on_action: Box<dyn FnOnce(&ConfirmationChoice, &Context) -> CliAction>,
     context: &Context,
 ) -> CliAction {
-    text_choice_input_display::handle(
-        text,
-        vec![
-            Choice {
-                id: 0,
-                text: String::from("Yes"),
-            },
-            Choice {
-                id: 1,
-                text: String::from("No"),
-            },
-        ],
-        Box::new(|choice, global_context| {
-            let confirmation_choice = if choice.id == 0 {
-                ConfirmationChoice::Yes
-            } else {
-                ConfirmationChoice::No
-            };
+    let confirmed = Confirm::new()
+        .with_prompt(text)
+        .interact()
+        .unwrap_or_default();
+    let confirmation_choice = if confirmed {
+        ConfirmationChoice::Yes
+    } else {
+        ConfirmationChoice::No
+    };
 
-            on_action(&confirmation_choice, global_context)
-        }),
-        context,
-    )
+    on_action(&confirmation_choice, context)
 }
