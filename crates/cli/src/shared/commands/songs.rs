69481bf18f639915d9d57c1e9a500d337f36c246
sleepyfran <fgzv@outlook.com>
2020-11-06 22:23:20 +0100

    :sparkles: Add song editing and discarding

--- crates/cli/src/shared/commands/songs.rs
+++ crates/cli/src/shared/commands/songs.rs
@@ -2,9 +2,12 @@ use std::collections::HashSet;
 use std::sync::Arc;
 
 use common::entities::Song;
+use common::extensions::option::OptionCloneExtensions;
+use game::music::songs;
 
 use super::Command;
-use crate::shared::action::CliAction;
+use crate::effects;
+use crate::shared::action::{Choice, CliAction, Prompt, PromptText};
 use crate::shared::display;
 use crate::shared::emoji;
 
@@ -20,19 +23,30 @@ songs
 When called with no paramaters will display all unfinished songs. Can also be called with:
 
 [edit] - Shows a list of songs to edit the name of
-[remove] - Shows a list of songs to remove
+[discard] - Shows a list of songs to remove
         "#
         .into(),
-        execute: Arc::new(move |_args, global_context| {
-            let unfinished_songs = &global_context.game_state.character.songs_in_progress;
-            show_songs(unfinished_songs);
+        execute: Arc::new(move |args, global_context| {
+            let unfinished_songs = global_context
+                .game_state
+                .character
+                .songs_in_progress
+                .clone();
 
-            CliAction::Continue
+            let command = args.first();
+            match command {
+                Some(command) => match command.as_str() {
+                    "edit" => edit_song(unfinished_songs),
+                    "discard" => discard_song(unfinished_songs),
+                    _ => show_songs(unfinished_songs),
+                },
+                _ => show_songs(unfinished_songs),
+            }
         }),
     }
 }
 
-fn show_songs(unfinished_songs: &HashSet<Song>) {
+fn show_songs(unfinished_songs: HashSet<Song>) -> CliAction {
     let max_column_separator = unfinished_songs
         .iter()
         .map(|song| song.name.len())
@@ -46,7 +60,9 @@ fn show_songs(unfinished_songs: &HashSet<Song>) {
         show_song(index, song, max_column_separator)
     }
     display::show_line_break();
-    display::show_text("You can finish any of them at any time by recording them");
+    display::show_text_with_new_line("You can finish any of them at any time by recording them");
+
+    CliAction::Continue
 }
 
 fn show_song(index: usize, song: &Song, name_padding: usize) {
@@ -63,6 +79,55 @@ fn show_song(index: usize, song: &Song, name_padding: usize) {
     ));
 
     display::show_line_break();
+}
+
+fn edit_song(unfinished_songs: HashSet<Song>) -> CliAction {
+    CliAction::Prompt(Prompt::ChoiceInput {
+        text: PromptText::WithEmoji("Which song do you want to edit?".into()),
+        choices: unfinished_songs
+            .iter()
+            .enumerate()
+            .map(|(index, song)| Choice {
+                id: index,
+                text: format!("{} (Quality: {})", song.name, song.quality),
+            })
+            .collect(),
+        on_action: Box::new(move |choice, _| {
+            let selected_song = unfinished_songs.iter().nth(choice.id).unwrap_cloned();
+
+            CliAction::Prompt(Prompt::TextInput {
+                text: PromptText::WithEmoji("What's the new name of the song?".into()),
+                on_action: Box::new(move |name, global_context| {
+                    let updated_state =
+                        songs::edit(selected_song, name.clone(), global_context).game_state;
+                    display::show_line_break();
+                    display::show_info(&format!("Song name changed to {}", name));
+                    effects::set_state(updated_state)
+                }),
+            })
+        }),
+    })
+}
+
+fn discard_song(unfinished_songs: HashSet<Song>) -> CliAction {
+    CliAction::Prompt(Prompt::ChoiceInput {
+        text: PromptText::WithEmoji("Which song do you want to discard?".into()),
+        choices: unfinished_songs
+            .iter()
+            .enumerate()
+            .map(|(index, song)| Choice {
+                id: index,
+                text: format!("{} (Quality: {})", song.name, song.quality),
+            })
+            .collect(),
+        on_action: Box::new(move |choice, global_context| {
+            let selected_song = unfinished_songs.iter().nth(choice.id).unwrap_cloned();
+            let updated_state = songs::discard(selected_song.clone(), global_context).game_state;
+            display::show_line_break();
+            display::show_info(&format!("Song {} removed", selected_song.name));
+            effects::set_state(updated_state)
+        }),
+    })
 }
 
 fn get_quality_emoji_for(song: &Song) -> String {
--- crates/game/src/lib.rs
+++ crates/game/src/lib.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate derive_builder;
 
+#[macro_use]
+extern crate lenses;
+
 extern crate strum;
 #[macro_use]
 extern crate strum_macros;
@@ -9,5 +12,6 @@ pub mod builders;
 pub mod constants;
 pub mod context;
 pub mod data;
+pub mod music;
 pub mod operations;
 pub mod world;
