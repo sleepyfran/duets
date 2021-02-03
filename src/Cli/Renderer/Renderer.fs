1fa0677cf69de1de9dca49624d593b4c23b03803
sleepyfran <fgzv@outlook.com>
2021-02-03 10:27:22 -0600

    :bulb: Add docs in Renderer

--- src/Cli/Renderer/Renderer.fs
+++ src/Cli/Renderer/Renderer.fs
@@ -3,10 +3,15 @@ module Renderer
 open Action
 open Spectre.Console
 
+/// Writes a message into the buffer.
 let renderMessage (message: string) =
   AnsiConsole.Write message |> ignore
   None
 
+/// Renders the specified prompt and asks the user for a response depending
+/// on the specified type of prompt. Returns a string which either represents
+/// the raw user input (in case of a TextPrompt) or the ID of the choice that
+/// the user chose (in case of a ChoicePrompt).
 let renderPrompt prompt =
   match prompt.Content with
   | TextPrompt ->
@@ -14,6 +19,8 @@ let renderPrompt prompt =
   | ChoicePrompt choices ->
       Some ((Array.head choices).Id)
 
+/// Renders an action and returns an option that encapsulates the user response
+/// to it.
 let render action =
   match action with
   | Message message -> renderMessage message
