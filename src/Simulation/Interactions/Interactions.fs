690a877308eb6ded0dc8b314869aa8f060b145e9
sleepyfran <fgzv@outlook.com>
2022-05-11 21:37:59 +0200

    :art: Better naming in interactions

--- src/Simulation/Interactions/Interactions.fs
+++ src/Simulation/Interactions/Interactions.fs
@@ -7,8 +7,9 @@ type InteractionResult =
     { Enabled: Interaction list
       Disabled: DisabledInteraction list }
 
-let private goOutFrom city destinationId =
-    let cityNode = Queries.World.Common.contentOf city.Graph destinationId
+let private goOutTo city destinationId =
+    let cityNode =
+        Queries.World.Common.contentOf city.Graph destinationId
 
     FreeRoamInteraction.GoOut(destinationId, cityNode)
     |> Interaction.FreeRoam
@@ -29,7 +30,7 @@ let private placeInteractions city placeId roomId place =
     let exitInteraction =
         place.Exits
         |> Map.tryFind roomId
-        |> Option.map (fun exitId -> [ goOutFrom city exitId ])
+        |> Option.map (fun exitId -> [ goOutTo city exitId ])
         |> Option.defaultValue []
 
     moveInteractions @ exitInteraction
@@ -45,17 +46,20 @@ let private concertSpaceInteractions room =
 /// be later transformed into the actual flow.
 /// </summary>
 let rec availableCurrently state =
-    let currentPosition = Queries.World.Common.currentPosition state
+    let currentPosition =
+        Queries.World.Common.currentPosition state
 
-    let defaultInteractions = [ Interaction.FreeRoam FreeRoamInteraction.Wait ]
+    let defaultInteractions =
+        [ Interaction.FreeRoam FreeRoamInteraction.Wait ]
 
     match currentPosition.Content with
     | ResolvedPlaceCoordinates coords ->
         let placeId, roomId = coords.Coordinates
 
         match coords.Place.Space with
-        | ConcertSpace space ->
-            let specificInteractions = concertSpaceInteractions coords.Room
+        | ConcertSpace _ ->
+            let specificInteractions =
+                concertSpaceInteractions coords.Room
 
             placeInteractions currentPosition.City placeId roomId coords.Place
             @ specificInteractions
