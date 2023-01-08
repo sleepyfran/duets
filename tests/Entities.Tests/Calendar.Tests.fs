1b152053aa7e03f708a03d5d3a2a14b9d05fd6f2
sleepyfran <fgzv@outlook.com>
2023-01-09 00:32:43 +0100

    :white_check_mark: Add Calendar tests

---
+++ tests/Entities.Tests/Calendar.Tests.fs
@@ -0,0 +1,36 @@
+module Entities.Tests.Calendar
+
+open FsUnit
+open Fugit.Months
+open NUnit.Framework
+
+open Entities
+
+let private baseDate =
+    January 9 2023
+
+(* ------ QUERY ------ *)
+
+[<Test>]
+let ``next should return next day moment`` () =
+    let next =
+        January 9 2023
+        |> Calendar.Transform.changeDayMoment EarlyMorning
+        |> Calendar.Query.next
+
+    next.Day |> should equal 9
+    next.Month |> should equal 1
+    next.Year |> should equal 2023
+    next |> Calendar.Query.dayMomentOf |> should equal Morning
+
+[<Test>]
+let ``next should return next day when next day moment is midnight`` () =
+    let next =
+        January 9 2023
+        |> Calendar.Transform.changeDayMoment Night
+        |> Calendar.Query.next
+    
+    next.Day |> should equal 10
+    next.Month |> should equal 1
+    next.Year |> should equal 2023
+    next |> Calendar.Query.dayMomentOf |> should equal Midnight
