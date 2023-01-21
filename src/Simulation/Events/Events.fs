79a66496d7cb02a157b6d4a0f3100e6d70e6d13c
sleepyfran <fgzv@outlook.com>
2023-01-22 00:42:53 +0100

    :sparkles: Lower attributes as time passes

--- src/Simulation/Events/Events.fs
+++ src/Simulation/Events/Events.fs
@@ -4,10 +4,7 @@ open Entities
 
 /// Retrieves all associated effects with the given one.
 let associatedEffects effect =
-    match effect with
-    | TimeAdvanced date -> [ Time.run date ]
-    | _ -> []
-    @ Skill.run effect @ Character.Character.run effect
+    Time.run effect @ Skill.run effect @ Character.Character.run effect
 
 /// Retrieves all the effects that have to happen at the end of an effect chain.
 let endOfChainEffects = [ Place.ClosingTime.checkCurrentPlace ]
--- src/Simulation/Events/Time.Events.fs
+++ src/Simulation/Events/Time.Events.fs
@@ -3,29 +3,35 @@ module Simulation.Events.Time
 open Entities
 open Simulation
 open Simulation.Calendar
+open Simulation.Character
 open Simulation.Market
 
-let private runYearlyEffects state time =
+let private runYearlyEffects time state =
     if Calendar.Query.isFirstMomentOfYear time then
         [ state.GenreMarkets |> GenreMarket.update ]
     else
         []
 
-let private runDailyEffects state time =
+let private runDailyEffects time state =
     match Calendar.Query.dayMomentOf time with
     | Morning ->
         Albums.DailyUpdate.dailyUpdate state
         |> (@) (Concerts.DailyUpdate.dailyUpdate state)
     | _ -> []
 
-let rec private runCurrentTimeChecks state time =
+let rec private runCurrentTimeChecks time state =
     Concerts.Scheduler.moveFailedConcerts state time
     @ Notifications.createHappeningSoon state time
+      @ Character.AttributeChange.applyAfterTimeChange state
 
 /// Runs all the events associated with the current time of the game, this
 /// includes all daily, yearly and current time effects such as updating the
 /// streams of albums, failing concerts that haven't happened, etc.
-let internal run time state =
-    runDailyEffects state time
-    |> (@) (runYearlyEffects state time)
-    |> (@) (runCurrentTimeChecks state time)
+let internal run effect =
+    match effect with
+    | TimeAdvanced time ->
+        [ runDailyEffects time
+          runYearlyEffects time
+          runCurrentTimeChecks time ]
+    | Wait times -> [ AttributeChange.applyAfterWait times ]
+    | _ -> []
