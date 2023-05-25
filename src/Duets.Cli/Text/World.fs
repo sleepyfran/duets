a6cee9dfc8f0b2f9b752fd8589c168d7b8f281d7
sleepyfran <fgzv@outlook.com>
2023-05-26 01:06:45 +0200

    :lipstick: Show custom studio description

--- src/Duets.Cli/Text/World.fs
+++ src/Duets.Cli/Text/World.fs
@@ -43,9 +43,26 @@ let private concertSpaceDescription (place: Place) =
                 "The concert venue is packed with excitement as you make your way in. The only sound is the roar of the crowd as they take their seats. You can't help but feel surprised that every single seat is filled and there are even people standing in the back, it seems like the crowd is way more than you were expecting. The stage is set up and ready to go, with instruments and equipment waiting to be played. The lights are dimmed, but you can see that the rows of seats in the auditorium are completely filled and there's not even an inch of empty space."
     | None -> defaultPlaceDescription place
 
+let studioSpaceDescription place =
+    match place.Quality with
+    | q when q < 30<quality> ->
+        Styles.Level.bad
+            "The studio is in a terrible state. The walls are crumbling, the floor is cracked, and the ceiling is leaking. It's a miracle that the place is still standing at all. The only thing that seems to be in okay condition is the equipment. The producer is smoking a cigarette and doesn't seem to be paying attention to you"
+    | q when q < 60<quality> ->
+        Styles.Level.normal
+            "The studio is in a decent state. It's not the best looking studio, but it's still a good place to record music. The equipment seems to be in a good condition, but it's not new and seems to have been used for a while. The producer is on its phone and doesn't seem to be paying attention to you"
+    | _ ->
+        Styles.Level.good
+            "The studio looks great, seems to have been recently renovated. The equipment looks new and shiny. The producer is waiting for you to start recording!"
+
+let studioPrice studio =
+    $"Each song will cost {Styles.money studio.PricePerSong} to record and produce"
+
 let placeDescription (place: Place) =
     match place.Type with
     | PlaceType.ConcertSpace _ -> concertSpaceDescription place
+    | PlaceType.Studio studio ->
+        $"{studioSpaceDescription place}\n{studioPrice studio}"
     | _ -> defaultPlaceDescription place
 
 let placeTypeName (placeType: PlaceTypeIndex) =
