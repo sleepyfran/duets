25f3b046632311a4e008f4a5c13f6bbeb82151a9
sleepyfran <fgzv@outlook.com>
2023-08-21 15:53:32 +0200

    :sparkles: Work within place opening hours
    Now the shifts will not last past the place opening hours.

--- src/Duets.Simulation/Careers/Work.fs
+++ src/Duets.Simulation/Careers/Work.fs
@@ -14,14 +14,37 @@ let private workAttributeChange state (job: Job) =
     |> List.collect (fun (attribute, amount) ->
         (attribute, amount * shiftDuration) ||> Attribute.add character)
 
+let private dayMomentsUntilClosingTime state job =
+    let currentTime = Queries.Calendar.today state
+    let jobPlace = job.Location ||> Queries.World.placeInCityById
+
+    match jobPlace.OpeningHours with
+    | PlaceOpeningHours.OpeningHours(_, dayMoments) ->
+        let closedDayMoment =
+            dayMoments |> List.last |> Calendar.Query.nextDayMoment
+
+        let closingTime =
+            currentTime |> Calendar.Transform.changeDayMoment closedDayMoment
+
+        Calendar.Query.dayMomentsBetween currentTime closingTime |> Some
+    | PlaceOpeningHours.AlwaysOpen -> None
+
+let private timeAdvancement state job =
+    let dayMomentsUntilClosing = dayMomentsUntilClosingTime state job
+    let shiftDayMoments = Career.jobDuration job
+
+    match dayMomentsUntilClosing with
+    | Some dayMoments -> min shiftDayMoments dayMoments
+    | None -> shiftDayMoments
+
 /// Starts a work shift in the given job, passing the necessary amount of day
 /// moments until the shift ends, paying the character the earned amount and
 /// reducing the needed attributes from the character.
 let workShift state job =
-    let shiftDayMoments = Career.jobDuration job
-    let timeEffects = AdvanceTime.advanceDayMoment' state shiftDayMoments
-
     let characterAccount = Queries.Bank.playableCharacterAccount state
+
+    let shiftDayMoments = timeAdvancement state job
+    let timeEffects = AdvanceTime.advanceDayMoment' state shiftDayMoments
 
     let shiftSalary =
         job.CurrentStage.BaseSalaryPerDayMoment * decimal shiftDayMoments
---
+++ tests/Simulation.Tests/Careers/Work.Test.fs
@@ -0,0 +1,81 @@
+module Duets.Simulation.Tests.Careers.Work
+
+open FsCheck
+open FsUnit
+open Fugit.Months
+open NUnit.Framework
+open Test.Common.Generators
+
+open Duets.Data
+open Duets.Entities
+open Duets.Simulation
+open Duets.Simulation.Careers
+
+let private place =
+    Queries.World.placesByTypeInCity Prague PlaceTypeIndex.Cafe |> List.head
+
+let private job =
+    { Id = Barista
+      CurrentStage = (Careers.BaristaCareer.stages |> List.head)
+      Location = Prague, place.Id }
+
+let morningTime = August 21 2023 |> Calendar.Transform.changeDayMoment Morning
+
+let private state =
+    State.generateOne State.defaultOptions |> State.Calendar.setTime morningTime
+
+[<TestFixture>]
+type ``When place is not near closing time``() =
+    [<Test>]
+    member _.``work pays the full payment for all the day moments worked``() =
+        let effect =
+            Work.workShift state job
+            |> List.filter (function
+                | MoneyEarned _ -> true
+                | _ -> false)
+            |> List.head
+
+        match effect with
+        | MoneyEarned(_, Incoming(_, amount)) -> amount |> should equal 14m<dd>
+        | _ -> failwith "Unexpected effect"
+
+    [<Test>]
+    member _.``work passes the number of shift day moments specified in the career stage when place is not near closing time``
+        ()
+        =
+        Work.workShift state job
+        |> List.filter (function
+            | TimeAdvanced _ -> true
+            | _ -> false)
+        |> should haveLength 2
+
+[<TestFixture>]
+type ``When place is near closing time``() =
+    let eveningTime = morningTime |> Calendar.Transform.changeDayMoment Evening
+
+    let stateInEvening = state |> State.Calendar.setTime eveningTime
+
+    [<Test>]
+    member _.``work pays only the worked day moments until the place will close``
+        ()
+        =
+        let effect =
+            Work.workShift stateInEvening job
+            |> List.filter (function
+                | MoneyEarned _ -> true
+                | _ -> false)
+            |> List.head
+
+        match effect with
+        | MoneyEarned(_, Incoming(_, amount)) -> amount |> should equal 7m<dd>
+        | _ -> failwith "Unexpected effect"
+
+    [<Test>]
+    member _.``work passes only the number of day moments until the place will close``
+        ()
+        =
+        Work.workShift stateInEvening job
+        |> List.filter (function
+            | TimeAdvanced _ -> true
+            | _ -> false)
+        |> should haveLength 1
