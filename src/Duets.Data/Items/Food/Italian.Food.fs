627d74db8285c7e57418c23bc7200231eb6abaf8
sleepyfran <fgzv@outlook.com>
2023-10-08 16:17:52 +0200

    :card_file_box: Improve italian dish list

--- src/Duets.Data/Items/Food/Italian.Food.fs
+++ src/Duets.Data/Items/Food/Italian.Food.fs
@@ -3,25 +3,53 @@ module Duets.Data.Items.Food.Italian
 open Duets.Entities
 
 let all: PurchasableItem list =
-    [ { Brand = "Margherita Pizza"
-        Type = Regular 300<gram> |> Food |> Consumable },
+    [ { Brand = "Pizza Margherita"
+        Type = Regular 600<gram> |> Food |> Consumable },
       8.5m<dd>
 
       { Brand = "Spaghetti Carbonara"
-        Type = Unhealthy 250<gram> |> Food |> Consumable },
+        Type = Regular 450<gram> |> Food |> Consumable },
       9.3m<dd>
 
+      { Brand = "Ravioli al Tartufo"
+        Type = Regular 450<gram> |> Food |> Consumable },
+      11.3m<dd>
+
+      { Brand = "Fettuccine Alfredo"
+        Type = Regular 450<gram> |> Food |> Consumable },
+      9.3m<dd>
+
+      { Brand = "Penne alla Vodka"
+        Type = Regular 450<gram> |> Food |> Consumable },
+      9.5m<dd>
+
+      { Brand = "Penne all'Arrabbiata"
+        Type = Regular 450<gram> |> Food |> Consumable },
+      9.0m<dd>
+
+      { Brand = "Spaghetti Bolognese"
+        Type = Regular 450<gram> |> Food |> Consumable },
+      9.2m<dd>
+
+      { Brand = "Penne alla Puttanesca"
+        Type = Regular 450<gram> |> Food |> Consumable },
+      9.1m<dd>
+
+      { Brand = "Gnocchi Quattro Formaggi"
+        Type = Regular 450<gram> |> Food |> Consumable },
+      9.0m<dd>
+
       { Brand = "Bruschetta"
-        Type = Healthy 150<gram> |> Food |> Consumable },
+        Type = Regular 150<gram> |> Food |> Consumable },
       5.0m<dd>
 
-      { Brand = "Lasagna"
+      { Brand = "Lasagna Bolognese"
         Type = Unhealthy 350<gram> |> Food |> Consumable },
       7.8m<dd>
 
-      { Brand = "Tiramisu"
-        Type = Unhealthy 150<gram> |> Food |> Consumable },
-      3.5m<dd>
+      { Brand = "Lasagna Vegetariana"
+        Type = Unhealthy 350<gram> |> Food |> Consumable },
+      7.2m<dd>
 
       { Brand = "Pesto Pasta"
         Type = Regular 300<gram> |> Food |> Consumable },
@@ -31,14 +59,14 @@ let all: PurchasableItem list =
         Type = Healthy 200<gram> |> Food |> Consumable },
       7.2m<dd>
 
-      { Brand = "Osso Buco"
-        Type = Regular 500<gram> |> Food |> Consumable },
-      4.3m<dd>
-
       { Brand = "Risotto alla Milanese"
         Type = Regular 350<gram> |> Food |> Consumable },
       4.6m<dd>
 
       { Brand = "Gelato"
-        Type = Regular 200<gram> |> Food |> Consumable },
-      1.5m<dd> ]
+        Type = Unhealthy 200<gram> |> Food |> Consumable },
+      1.5m<dd>
+
+      { Brand = "Tiramisu"
+        Type = Unhealthy 150<gram> |> Food |> Consumable },
+      3.5m<dd> ]
