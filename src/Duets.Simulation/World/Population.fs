783f5ad1f7c23e7a75bdbe4a91654f26a075e84e
sleepyfran <fgzv@outlook.com>
2023-12-14 14:00:18 +0100

    :sparkles: Add place-specific NPCs

--- src/Duets.Simulation/World/Population.fs
+++ src/Duets.Simulation/World/Population.fs
@@ -20,7 +20,9 @@ let private placePopulationRange (place: Place) =
     | Home -> 0, 0 (* Homes are private *)
     | Hotel _ -> 1, 5
     | Hospital -> 1, 2
-    | RehearsalSpace _ -> 1, 10
+    | RehearsalSpace _ ->
+        (* Rehearsal spaces will only be populated by the band. *)
+        0, 0
     | Restaurant -> 1, 10 (* TODO: Base this on the type of restaurant. *)
     | Studio _ ->
         (* Studios will only be populated by the producer and the band. *)
@@ -32,6 +34,18 @@ let private allKnownNpcs cityId state =
     |> List.map (fun relationship ->
         Queries.Characters.find state relationship.Character)
 
+let private generateForPlaceWithSpecificNpcs place state =
+    let bandMembers =
+        Queries.Bands.currentBandMembersWithoutPlayableCharacter state
+        |> List.map (_.CharacterId >> Queries.Characters.find state)
+
+    match place.PlaceType with
+    | RehearsalSpace _ ->
+        bandMembers
+    | Studio studio ->
+        studio.Producer :: bandMembers
+    | _ -> []
+
 /// Generates an effect that puts a random number of people in the given place,
 /// keeping in mind the place's population range, which depends on the place type
 /// and whether it is private or not (like a home or a studio).
@@ -39,28 +53,33 @@ let generateForPlace cityId place state =
     let numberOfPeople = placePopulationRange place ||> RandomGen.genBetween
     let knownNpcs = allKnownNpcs cityId state
 
-    [ 1..numberOfPeople ]
-    |> List.fold
-        (fun (npcsInRoom, knownNpcs) _ ->
-            let shouldPullFromKnownNpcs =
-                knownNpcs |> List.isNotEmpty
-                && RandomGen.chance
-                    Config.Population.chanceOfKnownPeopleAtPlace
+    let randomNpcs =
+        [ 1..numberOfPeople ]
+        |> List.fold
+            (fun (npcsInRoom, knownNpcs) _ ->
+                let shouldPullFromKnownNpcs =
+                    knownNpcs |> List.isNotEmpty
+                    && RandomGen.chance
+                        Config.Population.chanceOfKnownPeopleAtPlace
 
-            if shouldPullFromKnownNpcs then
-                (*
-                Pull a new character from the known NPCs and remove them from
-                the list of known NPCs so that they don't appear again.
-                *)
-                let randomIndex = RandomGen.sampleIndex knownNpcs
-                let randomNpc = knownNpcs |> List.item randomIndex
-                let knownNpcs = knownNpcs |> List.removeAt randomIndex
+                if shouldPullFromKnownNpcs then
+                    (*
+                    Pull a new character from the known NPCs and remove them from
+                    the list of known NPCs so that they don't appear again.
+                    *)
+                    let randomIndex = RandomGen.sampleIndex knownNpcs
+                    let randomNpc = knownNpcs |> List.item randomIndex
+                    let knownNpcs = knownNpcs |> List.removeAt randomIndex
 
-                (randomNpc :: npcsInRoom, knownNpcs)
-            else
-                (* Create a new NPC and keep the known list as is. *)
-                let randomNpc = Character.Npc.generateRandom state
-                (randomNpc :: npcsInRoom, knownNpcs))
-        ([], knownNpcs)
-    |> fst
+                    (randomNpc :: npcsInRoom, knownNpcs)
+                else
+                    (* Create a new NPC and keep the known list as is. *)
+                    let randomNpc = Character.Npc.generateRandom state
+                    (randomNpc :: npcsInRoom, knownNpcs))
+            ([], knownNpcs)
+        |> fst
+
+    let placeSpecificNpcs = generateForPlaceWithSpecificNpcs place state
+
+    randomNpcs @ placeSpecificNpcs
     |> WorldPeopleInCurrentRoomChanged
