96e90bd5a904fcc412a5f4d326d7774f1a864f9e
sleepyfran <fgzv@outlook.com>
2024-06-29 21:00:11 +0200

    :sparkles: Add cheats to time travel and increase band fans

--- src/Duets.Cli/Components/Commands/Cheats/Life.Cheats.Commands.fs
+++ src/Duets.Cli/Components/Commands/Cheats/Life.Cheats.Commands.fs
@@ -1,31 +1,35 @@
 namespace Duets.Cli.Components.Commands.Cheats
 
+open System
 open Duets.Agents
 open Duets.Cli
+open Duets.Cli.Components
 open Duets.Cli.Components.Commands
 open Duets.Cli.SceneIndex
 open Duets.Cli.Text
 open Duets.Entities
 open Duets.Simulation
+open Duets.Simulation.Time.AdvanceTime
 
 [<RequireQualifiedAccess>]
 module LifeCommands =
+    let private makeHappy () =
+        let character = Queries.Characters.playableCharacter (State.get ())
+
+        [ CharacterAttribute.Energy
+          CharacterAttribute.Health
+          CharacterAttribute.Hunger
+          CharacterAttribute.Mood ]
+        |> List.iter (fun attr ->
+            Character.Attribute.add character attr 100 |> Effect.applyMultiple)
+
     /// Command which gives 100 to all needs of the character.
     let happy =
         { Name = "happy"
           Description = "Sets energy, health, hunger and mood to 100%"
           Handler =
             (fun _ ->
-                let character =
-                    Queries.Characters.playableCharacter (State.get ())
-
-                [ CharacterAttribute.Energy
-                  CharacterAttribute.Health
-                  CharacterAttribute.Hunger
-                  CharacterAttribute.Mood ]
-                |> List.iter (fun attr ->
-                    Character.Attribute.add character attr 100
-                    |> Effect.applyMultiple)
+                makeHappy ()
 
                 Scene.Cheats) }
 
@@ -47,3 +51,26 @@ module LifeCommands =
                 |> Effect.apply
 
                 Scene.Cheats) }
+
+    /// Command which lets the user advance in time.
+    let timeTravel =
+        { Name = "time travel"
+          Description =
+            "Advances the time by the number of day moments specified in the argument, keeping your character happy"
+          Handler =
+            (fun times ->
+                "Travelling in time..." |> Styles.information |> showMessage
+
+                let joinedArgs = String.Join("", times)
+
+                match Int32.TryParse joinedArgs with
+                | true, n ->
+                    [ 1..n ]
+                    |> List.iter (fun _ ->
+                        makeHappy ()
+
+                        advanceDayMoment' (State.get ()) 1<dayMoments>
+                        |> Effect.applyMultiple)
+
+                    Scene.Cheats
+                | false, _ -> Scene.Cheats) }
