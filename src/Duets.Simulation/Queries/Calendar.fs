348697ab2a4cf333ea7d0fc595ee30a0fe471f5c
sleepyfran <fgzv@outlook.com>
2024-10-15 11:29:50 +0200

    Show time left on clock

--- src/Duets.Simulation/Queries/Calendar.fs
+++ src/Duets.Simulation/Queries/Calendar.fs
@@ -3,6 +3,7 @@ namespace Duets.Simulation.Queries
 module Calendar =
     open Aether
     open Duets.Entities
+    open Duets.Simulation
 
     /// Returns the current date in game.
     let today state = state |> Optic.get Lenses.State.today_
@@ -17,3 +18,20 @@ module Calendar =
 
         Seq.initInfinite (fun index ->
             currentDate |> Calendar.Query.nextN (index + 1))
+
+    /// Returns information about the current turn, including the current day
+    /// moment, the time spent in the current day moment, the duration of the
+    /// day moment and the time left in the current day moment.
+    let currentTurnInformation state =
+        let currentDayMoment = today state |> Calendar.Query.dayMomentOf
+        let nextDayMoment = currentDayMoment |> Calendar.Query.nextDayMoment
+
+        let currentTurnTime = state |> Optic.get Lenses.State.turnMinutes_
+
+        let dayMomentDuration = Config.Time.minutesPerDayMoment
+
+        {| CurrentDayMoment = currentDayMoment
+           NextDayMoment = nextDayMoment
+           TimeSpent = currentTurnTime
+           DayMomentDuration = dayMomentDuration
+           TimeLeft = dayMomentDuration - currentTurnTime |}
