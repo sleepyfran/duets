34c75212bdf43ae56514039c3ae5d02b06be21d2
sleepyfran <fgzv@outlook.com>
2024-10-15 11:38:49 +0200

    Adjust times

--- src/Duets.Simulation/Time/InteractionMinutes.fs
+++ src/Duets.Simulation/Time/InteractionMinutes.fs
@@ -8,17 +8,18 @@ let effectMinutes =
     function
     | AlbumStarted _
     | AlbumSongAdded _ -> 2 * Config.Time.minutesPerDayMoment
-    | Ate _ -> 10<minute>
-    | BookRead _ -> 45<minute>
+    | Ate _ -> 25<minute>
+    | BookRead _ -> 60<minute>
     | CareerShiftPerformed(_, shiftDuration, _) ->
         shiftDuration |> Calendar.DayMoments.toMinutes
     | ConcertFinished _ -> Config.Time.minutesPerDayMoment
-    | ConcertSoundcheckPerformed -> 30<minute>
-    | Drank _ -> 5<minute>
+    | ConcertSoundcheckPerformed -> 45<minute>
+    | Drank _ -> 15<minute>
+    | Exercised _ -> 60<minute>
     | FlightLanded flight ->
         Queries.Flights.flightTime flight |> Calendar.Seconds.toMinutes
-    | GamePlayed _ -> 25<minute>
-    | MerchStandSetup -> 25<minute>
+    | GamePlayed _ -> 30<minute>
+    | MerchStandSetup -> 30<minute>
     | MiniGamePlayed _ -> 30<minute>
     | SocialActionPerformed(_, actionKind) ->
         match actionKind with
@@ -28,7 +29,7 @@ let effectMinutes =
         | SocialActionKind.TellStory -> 30<minute>
     | SongImproved _
     | SongPracticed _
-    | SongStarted _ -> 90<minute>
-    | WatchedTv _ -> 15<minute>
+    | SongStarted _ -> 120<minute>
+    | WatchedTv _ -> 30<minute>
     | Wait dayMoments -> dayMoments |> Calendar.DayMoments.toMinutes
     | _ -> 0<minute>
