2c710f8f089ca9bffa3a423b4591f7cdf6e134b9
sleepyfran <fgzv@outlook.com>
2025-06-07 22:19:40 +0200

    :loud_sound: Improve logging system
    Messages are now saved in-memory while the game is still ongoing
    and written to the file when exitting. Also, we don't just keep
    appending forever and ever and simply replace the contents of the
    file every time that the game is closed. Farewell to those 20MB
    text files I've accumulated :^)

--- src/Duets.Agents/Log.fs
+++ src/Duets.Agents/Log.fs
@@ -7,12 +7,10 @@ open Duets.Entities
 type LogAgentMessage =
     | AppendEffect of Effect
     | AppendMessage of string
+    | DumpToFile of AsyncReplyChannel<unit>
 
-let private appendToLog (str: string) =
-    str |> (fun content -> $"{content}\n") |> Files.append (Files.logPath ())
-
-let private appendEffectToLog (effect: Effect) =
-    effect |> Serializer.serialize |> appendToLog
+let private writeToLog (messages: string list) =
+    messages |> String.concat "\n" |> Files.write (Files.logPath ())
 
 /// Agent in charge of writing to the activity log. See `Savegame.fs` for the
 /// reasoning behind have this operation and the savegame operations in an
@@ -21,21 +19,37 @@ type private LogAgent() =
     let agent =
         MailboxProcessor.Start
         <| fun inbox ->
-            let rec loop () =
+            let rec loop messages =
                 async {
                     let! msg = inbox.Receive()
 
                     match msg with
-                    | AppendEffect effect -> appendEffectToLog effect
-                    | AppendMessage message -> appendToLog message
+                    | AppendEffect effect ->
+                        return!
+                            (effect |> Serializer.serialize) :: messages |> loop
+                    | AppendMessage message ->
+                        return! $"{message}\n" :: messages |> loop
+                    | DumpToFile channel ->
+                        try
+                            writeToLog messages
+                        with _ ->
+                            (*
+                            The log file is just for debugging purposes, so
+                            we don't want to fail if the log file is not
+                            writable.
+                            *)
+                            ()
 
-                    return! loop ()
+                        channel.Reply()
+
+                    return! loop messages
                 }
 
-            loop ()
+            loop []
 
     member this.Append effect = effect |> AppendEffect |> agent.Post
     member this.Append str = str |> AppendMessage |> agent.Post
+    member this.DumpToFile() = agent.PostAndReply DumpToFile
 
 let private logAgent = LogAgent()
 
@@ -44,3 +58,6 @@ let appendEffect (effect: Effect) = logAgent.Append effect
 
 /// Attempts to write the given message to the activity log.
 let appendMessage (msg: string) = logAgent.Append msg
+
+/// Dumps the contents of the activity log to a file.
+let dumpToFileSync () = logAgent.DumpToFile()
